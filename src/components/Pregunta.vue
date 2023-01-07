283478f2b25e29dbb13fbb7616e178ff98fb0a6d
Fran-Jos <fjchanataxi@uce.edu.ec>
2023-01-07 10:49:58 -0500

    taller17

--- src/components/Pregunta.vue
+++ src/components/Pregunta.vue
@@ -1,27 +1,63 @@
 <template>
     <h1>Pregunta</h1>
-    <img src="https://via.placeholder.com/250" alt="no se puede visualizar">
+    <img v-if="imagsrc" v-bind:src="imagsrc" alt="no se puede visualizar">
   <div class="fondo-dark"></div>
   <div class="container">
-    <input type="text" placeholder="Hazme una pregunta">
+    <input v-model="question" type="text" placeholder="Hazme una pregunta">
     <p>Recuerda terminar con un signo de interrogacion (?)</p>
     <div>
-        <h2>Voy a pasar de año?</h2>
-        <h1>si, no, tal vez...</h1>
+        <h2>{{question}}</h2>
+        <h1>{{respuesta}}</h1>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            question: 'hola mundo',
+            respuesta:null,
+            imagsrc: null
+        }
+    },
+    watch:{
+        question(value, oldValue){
+            console.log(value);
+            if(!value.includes("?"))return;
+              console.log('vamos consimir API')  
+        this.consumirAPI()
+        }
 
-}
+    },
+    methods:{
+       async consumirAPI(){
+        this.respuesta='Pensando.'
+        this.respuesta='Pensando..'
+            //const data=await fetch('https://yesno.wtf/api').then(r=>r.json())
+            //desestructurado
+            const {answer, forced, image}=await fetch('https://yesno.wtf/api').then(r=>r.json())
+           
+            console.log(answer)
+            console.log(forced)
+            console.log(image)
+            this.respuesta='Pensando...'
+            this.respuesta=answer
+            this.imagsrc=image
+        }
+    }
+};
 </script>
 
 <style>
-    img{
+    img, .fondo-dark{
         height: 100vh;
         width: 100vw;
+        position: fixed;
+        top: 0px; left: 0px;
+    }
+    .fondo-dark{
+        background-color: rgba(0,0,0,0.5);
     }
     input{
         width: 250px;
@@ -40,5 +76,9 @@ export default {
     }
     h2{
         margin-top: 150px;
+    }
+    .container{
+        position: relative;
+        z-index: 99;
     }
 </style>
